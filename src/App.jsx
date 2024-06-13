d0029c89d0ab910aa272d11e2ad619168742394c
Jun-K-code <JunCode@aliyun.com>
2024-06-13 23:24:14 +0800

    feat: 实现useState和useReducer(未完成)

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,53 @@
-import React from './lib/react/React';
+import { useState } from 'react';
+// import React from './lib/react/React';
 
-// function App({ id }) {
-//   return (
-//     <div className="container" id={id}>
-//       <div className="one">
-//         <div className="two">
-//           <p>1</p>
-//           <p>2</p>
-//         </div>
-//         <div className="three">
-//           <p>3</p>
-//           <p>4</p>
-//         </div>
-//       </div>
-//       <p>this is a tes1</p>
-//     </div>
-//   );
-// }
+function App({ id }) {
+  const [count, setCount] = useState(0);
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="container" id={this.props.id}>
-        <div className="one">
-          <div className="two">
-            <p>111</p>
-            <p>222</p>
-          </div>
-          <div className="three">
-            <p>333</p>
-            <p>444</p>
-          </div>
+  return (
+    <div className="container" id={id}>
+      <div className="one">
+        <div className="two">
+          <p>1</p>
+          <p>2</p>
         </div>
-        <p>this is a tes!!!</p>
+        <div className="three">
+          <p>3</p>
+          <p>4</p>
+        </div>
       </div>
-    );
-  }
+      <p>this is a tes1</p>
+      <div>
+        <button onClick={() => setCout(count - 1)}>-</button>
+        <span>{count}</span>
+        <button onClick={() => setCout(count + 1)}>+</button>
+      </div>
+    </div>
+  );
 }
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <div className="container" id={this.props.id}>
+//         <div className="one">
+//           <div className="two">
+//             <p>111</p>
+//             <p>222</p>
+//           </div>
+//           <div className="three">
+//             <p>333</p>
+//             <p>444</p>
+//           </div>
+//         </div>
+//         <p>this is a tes!!!</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
